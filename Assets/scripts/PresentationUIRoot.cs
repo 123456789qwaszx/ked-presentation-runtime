a3be3fa37e7817abfed8acb17140c870f0cc968c
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-04-23 13:32:35 +0900

    feat: PresentationUIRoot 계층구조 제작

---
+++ Assets/scripts/PresentationUIRoot.cs
@@ -0,0 +1,45 @@
+public class PresentationUIRoot : UIRoot<PresentationUIRoot.Refs>
+{
+    public enum Refs
+    {
+        FullscreenFade_Root,
+        Letterbox_Root,
+        Flash_Root,
+        ScreenOverlay_Root,
+
+        StageShot_Root,
+        ShagePan_Root,
+        StageZoom_Root,
+        Stage_Root,
+        BackgroundSystem_Root,
+        BGShot_Root,
+        BGContent_Root,
+
+        BGOverlay_Root,
+
+        CharacterSystem_Root,
+        CharSlotLeft_Root,
+        CharSlotLeftFocus_Root,
+        CharSlotLeftRig_Root,
+
+        CharSlotCenter_Root,
+        CharSlotCenterFocus_Root,
+        CharSlotCenterRig_Root,
+
+        CharSlotRight_Root,
+        CharSlotRightFocus_Root,
+        CharSlotRightRig_Root,
+
+        Foreground_Root,
+
+        DialogueUI_Root,
+        DialogueBox_Root,
+        NameBox_Root,
+        NarrationBox_Root,
+
+        Choice_Root,
+        SystemUI_Root
+    }
+
+    
+}
