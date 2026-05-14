5e6a844b4fad3a8d5a813c4e9e4eb91685cc7f91
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-05-15 02:16:58 +0900

    chore: 연출 스크립트 정리

--- Assets/@Scripts/YarnCommandBridge/YarnCommandBridge.cs
+++ Assets/@Scripts/YarnCommandBridge/YarnCommandBridge.cs
@@ -52,7 +52,7 @@ public sealed partial class YarnCommandBridge : MonoBehaviour
         _dialogueRunner.AddCommandHandler<string, string>("size", EnqueueSetOriginSizeSpec);
         
         _dialogueRunner.AddCommandHandler<string, float, float>("move_by", EnqueueMoveBySpec);
-        _dialogueRunner.AddCommandHandler<string, float, float>("to_scale", EnqueueScaleToSpec);
+        _dialogueRunner.AddCommandHandler<string, float, float>("scale_to", EnqueueScaleToSpec);
 
         _dialogueRunner.AddCommandHandler<string, string>("slide_in", EnqueueSlideInSpec);
         _dialogueRunner.AddCommandHandler<string, string>("slide_out", EnqueueSlideOutSpec);
