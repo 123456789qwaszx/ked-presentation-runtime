0629637daf979657ef53842031be64d751c4f869
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-05-25 14:10:01 +0900

    refactor: UIBindingContext 제너릭 메서드 추출

--- Assets/@Scripts/UI/Core/UIBindingContext.cs
+++ Assets/@Scripts/UI/Core/UIBindingContext.cs
@@ -1,57 +1,29 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public sealed class UIBindingContext : IDisposable
 {
-    private readonly Dictionary<UIBase, List<Action>> _cleanupByOwner = new();
-    
+    private readonly OwnerCleanupContext<UIBase> _cleanups = new(Debug.LogException);
+
     public void AddBinding<T>(T owner, Action<T> bind, Action<T> unbind)
         where T : UIBase
     {
         bind(owner);
-        AddCleanup(owner, () => unbind(owner));
-    }
-    
-    private void AddCleanup(UIBase owner, Action cleanup)
-    {
-        if (!_cleanupByOwner.TryGetValue(owner, out var cleanups))
-        {
-            cleanups = new List<Action>();
-            _cleanupByOwner[owner] = cleanups;
-        }
-
-        cleanups.Add(cleanup);
+        _cleanups.AddCleanup(owner, () => unbind(owner));
     }
 
     public void Unbind(UIBase owner)
     {
-        if (!_cleanupByOwner.TryGetValue(owner, out var cleanups))
-            return;
-
-        for (int i = cleanups.Count - 1; i >= 0; i--)
-        {
-            try { cleanups[i]?.Invoke(); }
-            catch (Exception e) { Debug.LogException(e); }
-        }
-
-        _cleanupByOwner.Remove(owner);
+        _cleanups.Clear(owner);
     }
 
     public void UnbindAll()
     {
-        foreach (var kv in _cleanupByOwner)
-        {
-            List<Action> list = kv.Value;
-            for (int i = list.Count - 1; i >= 0; i--)
-            {
-                try { list[i]?.Invoke(); }
-                catch (Exception e) { Debug.LogException(e); }
-            }
-        }
-
-        _cleanupByOwner.Clear();
+        _cleanups.ClearAll();
     }
-    
-    public void Dispose() => UnbindAll();
+
+    public void Dispose()
+    {
+        _cleanups.Dispose();
+    }
 }
