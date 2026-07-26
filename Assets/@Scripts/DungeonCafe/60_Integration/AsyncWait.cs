e1401d3a4ca6e8e38882059ccc2e8eed23b63162
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-07-27 00:50:19 +0900

    chore: 레거시 코드 삭제 및 스크립트 이름 정정

--- Assets/@Scripts/DungeonCafe/60_Integration/AsyncWait.cs
+++ Assets/@Scripts/DungeonCafe/60_Integration/AsyncWait.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Threading.Tasks;
 
-/// <summary>
-/// 조건이 충족될 때까지 비동기적으로 대기한다.
-/// 특정 프레임이나 UI 상태를 폴링해야 할 때만 사용한다.
-/// </summary>
 public static class AsyncWait
 {
     public static async Task UntilAsync(Func<bool> predicate)
