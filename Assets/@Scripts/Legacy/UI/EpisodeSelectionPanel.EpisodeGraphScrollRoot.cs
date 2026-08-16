4930b799259b2c9ffb9f85f521ef12116c0b3a0b
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-08-17 00:36:32 +0900

    chore: 레거시 스크립트 전제 삭제

--- Assets/@Scripts/Legacy/UI/EpisodeSelectionPanel.EpisodeGraphScrollRoot.cs
+++
@@ -1,9 +0,0 @@
-// using UnityEngine;
-// using UnityEngine.UI;
-//
-// public sealed partial class EpisodeSelectionPanel : IEpisodeGraphScrollRootProvider
-// {
-//     public ScrollRect GraphScrollRect => View?.Rect(Refs.ButtonViewport)?.GetComponent<ScrollRect>();
-//     public RectTransform GraphContent => View?.Rect(Refs.EpisodeButtons);
-//     public RectTransform GraphViewport => View?.Rect(Refs.ButtonViewport);
-// }
