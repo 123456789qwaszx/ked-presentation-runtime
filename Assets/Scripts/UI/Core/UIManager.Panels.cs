d140f69a150ec3efe5d7018b93dad7e2544afb80
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-09-14 15:08:12 +0900

    refactor: close panel page with panel lifecycle

--- Assets/Scripts/UI/Core/UIManager.Panels.cs
+++ Assets/Scripts/UI/Core/UIManager.Panels.cs
@@ -43,6 +43,7 @@ public partial class UIManager
         // 숨기기 전에 티켓을 무효화한다. 로더가 비동기가 되면 이미 숨긴 UI에 패치가 적용될 수 있다.
         BumpShowVersion();
 
+        ClosePage(popped, afterPopped);
         HideManagedUI(popped);
 
         afterPopped?.Invoke(popped);
@@ -63,6 +64,7 @@ public partial class UIManager
         {
             UIBase popped = _panelStack.Pop();
 
+            ClosePage(popped, afterPopped);
             HideManagedUI(popped);
             afterPopped?.Invoke(popped);
         }
